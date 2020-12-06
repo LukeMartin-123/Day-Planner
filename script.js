1928e3236bc8f17bc9a46abbdab58e83e8ac8a35
Luke Martin <ljmart02@gmail.com>
2020-12-06 13:30:31 -0700

    Pseudo code added for js + rows in HTML

--- script.js
+++ script.js
@@ -1,6 +1,82 @@
 const now = moment();
+let day = [
+    {
+        hour: "9:00",
+
+        abbrieviation: "am",
+        reminder: ""
+    }
+
+    {
+        hour: "10:00",
+        abbrieviation: "am",
+        reminder: ""
+    }
+
+    {
+        hour: "11:00",
+        abbrieviation: "am",
+        reminder: ""
+    }
+
+    {
+        hour: "12:00",
+        abbrieviation: "pm",
+        reminder: ""
+    }
+
+    {
+        hour: "1:00",
+        abbrieviation: "pm",
+        reminder: ""
+    }
+
+    {
+        hour: "2:00",
+        abbrieviation: "pm",
+        reminder: ""
+    }
+
+    {
+        hour: "3:00",
+        abbrieviation: "pm",
+        reminder: ""
+    }
+
+    {
+        hour: "4:00",
+        abbrieviation: "pm",
+        reminder: ""
+    }
+
+    {
+        hour: "5:00",
+        abbrieviation: "pm",
+        reminder: ""
+    }
+
+
+
+
+
+
+
+
+
+
+
+]
+
+
+
+
+
 
 $("#currentDay").text(now.format("dddd MMMM Do YYYY"))
+
+
+
+
 
 
 
